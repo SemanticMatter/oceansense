f1dd9e64d9bbb7d18f5f9cf1472162a7a7bdf15e
Thomas Hagelien <thomas.f.hagelien@sintef.no>
2026-01-14 10:57:36 +0100

    Modified header button order

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -10,6 +10,11 @@
           <i class="fa-solid fa-compass"></i> Explorer
         </button>
       </RouterLink>
+      <RouterLink to="/datadoc" custom v-slot="{ navigate, isActive }">
+        <button :class="{ active: isActive }" @click="navigate">
+          <i class="fa-solid fa-file-alt"></i> Data Documentation
+        </button>
+      </RouterLink>
       <RouterLink to="/node" custom v-slot="{ navigate, isActive }">
         <button :class="{ active: isActive }" @click="navigate">
           <i class="fa-solid fa-network-wired"></i> Node Registry
@@ -18,11 +23,6 @@
       <RouterLink to="/api_view" custom v-slot="{ navigate, isActive }">
         <button :class="{ active: isActive }" @click="navigate">
           <i class="fa-solid fa-code"></i> Data Access & API
-        </button>
-      </RouterLink>
-      <RouterLink to="/datadoc" custom v-slot="{ navigate, isActive }">
-        <button :class="{ active: isActive }" @click="navigate">
-          <i class="fa-solid fa-file-alt"></i> Data Documentation
         </button>
       </RouterLink>
       <RouterLink to="/guidelines" custom v-slot="{ navigate, isActive }">
